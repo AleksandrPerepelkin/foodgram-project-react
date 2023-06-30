05df84888bceb88ce3225c98482a6ccfdd7abe38
AleksandrPerepelkin <wuld@mail.ru>
2023-06-30 13:56:17 +0300

    30/06

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-key')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['wuldpwnz.ddns.net', 'localhost', '158.160.16.142']
+ALLOWED_HOSTS = ['wuldpwnz.ddns.net', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
